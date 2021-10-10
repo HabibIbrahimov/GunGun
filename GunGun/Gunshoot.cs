523e5d1d2297ce5042aa52921643401b1bc35f5d
HabibIbrahimov <habibyi@code.edu.az>
2021-10-10 22:52:54 +0400

    Add project files.

---
+++ GunGun/Gunshoot.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GunGun
+{
+    class Gunshoot
+    {
+        public int Ammocount;
+        public int Personammo;
+        public int Magazinelimit; 
+        public string Firemode;
+
+        public void Gun(int personammo)
+        {
+            Ammocount = 30;
+            Personammo = personammo;
+            Magazinelimit = 30;
+            
+
+            
+            
+        }
+        public void Shoot()
+        {
+            if (Firemode=="one click")
+            {
+                if (Ammocount>0)
+                {
+                    Ammocount -= 1;
+                }
+                else
+                {
+                    Console.WriteLine("Don't shoot");
+                }
+            }
+            if (Firemode=="Automatic")
+            {
+                if (Ammocount>0)
+                {
+                    Ammocount = 0;
+                }
+            }
+        }
+
+        public void Reload()
+        {
+            if (Ammocount==0)
+            {
+               Ammocount = Magazinelimit;
+               Personammo -=Magazinelimit;
+            }
+            else
+            {
+                Console.WriteLine("Wait until the comb is empty");
+            }
+
+        }
+       
+        
+       
+    }
+
+    
+        
+       
+
+}
---
+++ GunGun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GunGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gunshoot AKM47 = new Gunshoot();
+            AKM47.Gun(100);
+            AKM47.Shoot();
+            AKM47.Reload();
+            Console.WriteLine(AKM47.Ammocount);
+           
+        }
+    }
+}
